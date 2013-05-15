9013bf36dc277ea313e1b44020f9a110910f260c
Ted Aronson <ted@mindsnacks.com>
2013-05-14 20:17:36 -0700

    temp profile page

--- emubaby/urls.py
+++ emubaby/urls.py
@@ -21,4 +21,5 @@ urlpatterns = patterns('',
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	(r'accounts/', include('registration.backends.default.urls')),
+	(r'accounts/profile/', frontend.views.profile_page)
 )
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,10 @@
-from django.template.response import SimpleTemplateResponse
+from django.template.response import SimpleTemplateResponse, TemplateResponse
 # Create your views here.
 
 
 def index(request):
 	return SimpleTemplateResponse('default_body.html')
+
+
+def profile_page(request):
+	return TemplateResponse(request, 'profile_page.html', {'user': request.user})

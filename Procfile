270578b62849e8b6df54aa367b8bb37e72eafee8
Ted Aronson <ted@mindsnacks.com>
2013-06-03 17:16:03 -0700

    Procfile runs collect static, and explicitly uses heroku settings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn emubaby.wsgi
+web: python manage.py collectstatic --noinput; gunicorn_django --bind=0.0.0.0:$PORT emubaby/settings/heroku.py
--- emubaby/settings/base.py
+++ emubaby/settings/base.py
@@ -165,18 +165,3 @@ LOGGING = {
 ACCOUNT_ACTIVATION_DAYS = 7
 
 ORG_NAME=os.environ.get("ORG_NAME", "MindSnacks")
-
-if os.environ.get("HEROKU_DEPLOY", False):
-        
-
-    # Parse database configuration from $DATABASE_URL
-    import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    EMAIL_HOST = 'localhost'
-    EMAIL_PORT = 1025
-
-    SECRET_KEY = os.environ['SECRET_KEY']

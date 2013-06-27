820b867a8316cd0427879cae1db9ceac6daf5ee8
Ted Aronson <ted@mindsnacks.com>
2013-06-27 11:46:52 -0700

    return the right status code when tests fail

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,5 @@
-coverage run --omit="venv/*,registration/*,*migrations/*,*tests*" manage.py test --settings=emubaby.settings.testing
+coverage run --omit="venv/*,*/virtualenv/*,registration/*,*migrations/*,*tests*" manage.py test --settings=emubaby.settings.testing
+TESTING_STATUS=$?
 coverage html
 coverage report
+exit $TESTING_STATUS

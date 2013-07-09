315cac34aa7ae22fd7b5e632da0d7c09e9669f72
Ted Aronson <ted@mindsnacks.com>
2013-07-09 11:22:27 -0700

    only do a travis deploy from master pushes

---
+++ utils/travis_deploy.sh
@@ -0,0 +1,11 @@
+if [[ $TRAVIS_BRANCH == 'master' ]]
+	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+	git remote add heroku git@heroku.com:alexandria-demo.git
+	echo "Host heroku.com" >> ~/.ssh/config
+	echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+	echo "   CheckHostIP no" >> ~/.ssh/config
+	echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config 
+	heroku keys:clear
+	yes | heroku keys:add
+	heroku run python manage.py migrate --settings=emubaby.settings.heroku
+fi

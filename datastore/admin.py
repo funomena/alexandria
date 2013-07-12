f8a33ae8c9b6c74a3cc6d4b82c20c140a43444b6
Ted Aronson <ted@mindsnacks.com>
2013-07-12 14:04:01 -0700

    metadata admin filtering

--- datastore/admin.py
+++ datastore/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin 
+from django.utils.translation import ugettext_lazy as _
 from datastore.models import *
+from datetime import date, timedelta
 
 
 def metadata_display_function(metadata_category):
@@ -11,6 +13,65 @@ def metadata_display_function(metadata_category):
 	return display_function
 
 
+class CreatedListFilter(admin.SimpleListFilter):
+	title = _('Created')
+
+	parameter_name = 'created'
+
+	def lookups(self, request, model_admin):
+		return (
+			('today', _('Today')),
+			('yesterday', _('Yesterday')),
+			('week', _('Past Week')),
+			('month', _('Past Month')),
+			('-7day', _('Older than a week')),
+			('-30day', _('Older than a month')),
+		)
+	
+	def queryset(self, request, queryset):
+
+		today = date.today()
+		if self.value() == 'today':
+			return queryset.filter(created__startswith=today)
+		elif self.value() == 'yesterday':
+			return queryset.filter(created__startswith=(today - timedelta(days=1)))
+		elif self.value() == 'week':
+			return queryset.filter(created__gte=(today - timedelta(days=7)))
+		elif self.value() == 'month':
+			return queryset.filter(created__gte=(today - timedelta(days=30)))
+		elif self.value() == '-7day':
+			return queryset.filter(created__lte=(today - timedelta(days=7)))
+		elif self.value() == '-30day':
+			return queryset.filter(created__lte=(today - timedelta(days=30)))
+
+		return queryset
+
+
+def metadata_filter_function(metadata_category):
+	class MetadataListFilter(admin.SimpleListFilter):
+		title = _(metadata_category.friendly_name)
+
+		parameter_name = metadata_category.slug
+
+		def lookups(self, request, model_admin):
+			metadata_list = []
+			for val in metadata_category.values.all():
+				metadata_list.append((val.value, _(val.value)))
+
+			metadata_list = tuple(metadata_list)
+			return metadata_list
+		
+		def queryset(self, request, queryset):
+			try:
+				queriedMetadata = MetaData.objects.get(value=self.value(), category=metadata_category)
+			except:
+				pass
+			else:
+				return queryset.filter(metadata__in=[queriedMetadata.pk])
+
+	return MetadataListFilter
+
+
 class BuildAdmin(admin.ModelAdmin):
 	def generate_list_display():
 		all_meta_categories = MetaDataCategory.objects.all()
@@ -19,7 +80,15 @@ class BuildAdmin(admin.ModelAdmin):
 			display = display + (metadata_display_function(cat),)
 		return display
 
+	def generate_list_filter():
+		all_meta_categories = MetaDataCategory.objects.all()
+		l_filter = (CreatedListFilter,)
+		for cat in all_meta_categories:
+			l_filter = l_filter + (metadata_filter_function(cat),)
+		return l_filter
+
 	list_display = generate_list_display()
+	list_filter = generate_list_filter()
 
 
 admin.site.register(Build, BuildAdmin)

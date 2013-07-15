9f1b781c3a990cdb1dc85e8172efc9ddb03940b0
Ted Aronson <ted@mindsnacks.com>
2013-07-15 14:07:52 -0700

    enable using external email hosts on heroku

--- alexandria/settings/heroku.py
+++ alexandria/settings/heroku.py
@@ -7,7 +7,9 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", "1025")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "alexandria")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "alexandria")
 
 SECRET_KEY = os.environ['SECRET_KEY']

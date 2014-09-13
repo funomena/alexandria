b2ed6ccca54cbc2903751797e120612fe23a9b90
Ted Aronson <ted@funomena.com>
2014-09-12 17:44:41 -0700

    special settings for setup

--- alexandria/settings.py
+++ alexandria/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qo)l&xmj1&c_@yuiuxg0z*a$x_efi08_=*ipw+4-a5n=9b=q+='
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ alexandria/setup_settings.py
@@ -0,0 +1,20 @@
+"""
+Django settings for alexandria project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import alexandria.settings
+
+INSTALLED_APPS = (
+    'datastore',
+    'registration',
+    'rest_framework',
+    'rest_framework.authtoken'
+) 

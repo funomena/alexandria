0c7e9005715a8f64947ed4c414e0899b5a203310
Ted Aronson <ted@funomena.com>
2014-09-16 15:29:10 -0700

    use full path as staticroot

--- alexandria/settings.py
+++ alexandria/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 STATIC_URL = '/static/'
 

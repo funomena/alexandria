e649f35f9fadd0c978f72802c6840a191654766b
Ted Aronson <ted@funomena.com>
2014-09-16 14:31:38 -0700

    turn off debugging on prod

--- alexandria/settings.py
+++ alexandria/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True" 
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 

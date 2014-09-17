8cacc0d0291c546bd4e45c7886ec35bac10719f7
Ted Aronson <ted@funomena.com>
2014-09-16 17:51:10 -0700

    add profiling tool

--- alexandria/settings.py
+++ alexandria/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar.apps.DebugToolbarConfig',
     'registration',
     'rest_framework',
     'rest_framework.authtoken'
